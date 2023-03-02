5458e0c403c8e183db3e00b9f4d690b2366a062e
Hasan Tatarlı <hasantatarli@gmail.com>
2023-03-02 15:29:22 +0300

    Added add_word_new function considering the documents structure changes

--- Words.py
+++ Words.py
@@ -33,10 +33,11 @@ class Vocabulary:
                 #     # word  = column[1]
                 print(lng,wrd,wrd_type,spl,exp)
 
-                self.add_word(lng,wrd,spl,tr,eng,wrd_type,exp,None,None,None,None)
+                self.add_word(lng,wrd,spl,tr,eng,wrd_type,exp)
                     #print(lng,word)
 
-    def add_word(self,language, word, spelling, turkish_meaning, english_meaning, word_type,usage, first_learning_date, first_repeat, second_repeat, third_repeat):
+    #def add_word(self,language, word, spelling, turkish_meaning, english_meaning, word_type,usage, first_learning_date, first_repeat, second_repeat, third_repeat):
+    def add_word(self,language, word, spelling, turkish_meaning, english_meaning, word_type,usage):
         document = {
             'Language': language,
             'Word': word,
@@ -45,21 +46,79 @@ class Vocabulary:
             'Turkish Meaning': turkish_meaning,
             'English Meaning': english_meaning,
             'Usage':usage,
-            'First Learning Date': first_learning_date,
-            'First Repeat':first_repeat,
-            'Second Repeat' : second_repeat,
-            'Third Repeat' : third_repeat,
-            'Date added' : datetime.datetime.now()
+            # 'First Learning Date': first_learning_date,
+            # 'First Repeat':first_repeat,
+            # 'Second Repeat' : second_repeat,
+            # 'Third Repeat' : third_repeat,
+            'Date added' : datetime.now()
         }
         return self.vocabularies.insert_one(document)
 
+    def add_word_new(self,language,translate_lang, word,word_type, spelling,meaning, usage, addedby):
+
+        word = {
+                'Word' : word,
+                'Language' :  language,
+                'Spelling':spelling,
+                'WordType':word_type,
+                'Translate': {
+                    'Language' : translate_lang,
+                    'Meaning':meaning,
+                    'UsageExample':usage
+                    }
+                ,
+                'DateAdded':datetime.now(),
+                'AddedBy':addedby
+                }
+        
+        print(word)
+        return self.vocabularies.insert_one(word)
 
 
     def delete_word(self):
         self.vocabularies.delete_many({})
 
-    def update_word():
-        return 0
+    def update_word(self, lang, word,wt=None, spl=None, tm=None, em=None, usa=None, nw_lang=None,nw_word=None,nw_wt=None, nw_spl=None, nw_tm=None, nw_em=None, nw_usa=None):
+        filter = {}
+        new_values = {}
+           
+
+        if (lang != None):
+            filter.update({"Language":lang})
+        if (word != None):
+            filter.update({"Word":word})            
+        if (wt != None):
+            filter.update({"Word Type":wt})
+        if (spl != None):
+            filter.update({"Spelling":spl})
+        if (tm != None):
+            filter.update({"Turkish Meaning":tm})
+        if (em != None):
+            filter.update({"English Meaning":em})  
+        if (usa != None):
+            filter.update({"Usage":usa})                                                                                                                                                                                              
+
+        if (nw_lang != None):
+            new_values.update({"Language":nw_lang})
+        if (nw_word != None):
+            new_values.update({"Word":nw_word})            
+        if (nw_wt != None):
+            new_values.update({"Word Type":nw_wt})
+        if (nw_spl != None):
+            new_values.update({"Spelling":nw_spl})
+        if (nw_tm != None):
+            new_values.update({"Turkish Meaning":nw_tm})
+        if (nw_em != None):
+            new_values.update({"English Meaning":nw_em})  
+        if (nw_usa != None):
+            new_values.update({"Usage":nw_usa})
+
+        new_values = { "$set": new_values  }
+        
+        
+        print(filter)
+        print(new_values)
+        result = self.vocabularies.update_one(filter,new_values)
         
     def update_first_learning_date(self,language, word):
         filter = {"Language": language, "Word": word}
@@ -112,15 +171,15 @@ class Vocabulary:
             print("######################")
 
 
-    def get_daily_words(self):
-        self.get_new_words(self)
 
+    # Daily New Words
+    # Criteria: First Learning Date is null
     def get_new_words(self):
-        results = self.vocabularies.aggregate([{ '$match': { 'First Learning Date': None }}, {'$sample': {'size': 5}}])
+        results = self.vocabularies.aggregate([{ '$match': { 'First Learning Date': None, 'Word': 'book'}}, {'$sample': {'size': 5}}])
 
         for datas in results:
             print("######################")
-            # print(datas)
+            print(datas)
             lang = datas.get("Language")
             word = datas.get("Word")
             
@@ -133,6 +192,8 @@ class Vocabulary:
             print("######################")
 
 
+    # Daily Firstly repeat words
+    # Criteria: First Learning Date is not null, First Repeat is null and First Learning Date
     def get_first_repeat_words(self):
         #results = self.vocabularies.aggregate([{'$match': { 'First Learning Date': { '$ne' : True}, 'First Repeat' : None }}, {'$sample': {'size': 5}}])
         results = self.vocabularies.aggregate([
@@ -141,7 +202,68 @@ class Vocabulary:
                 'First Learning Date': {
                     '$ne': None
                 }, 
-                'First Repeat': None
+                'First Repeat': None,
+                'Word':'book'
+            }
+        }, {
+            '$project': {
+                '_id': 0, 
+                'Language': 1, 
+                'Word': 1, 
+                'Word Type': 1, 
+                'Spelling': 1, 
+                'Turkish Meaning': 1, 
+                'English Meaning': 1, 
+                'Usage': 1, 
+                'First Learning Date': 1, 
+                'First Repeat': 1, 
+                'Second Repeat': 1, 
+                'Third Repeat': 1, 
+                'daysince': {
+                    '$divide': [
+                        {
+                            '$subtract': [
+                                datetime.combine(date.today(),datetime.min.time()) , '$First Learning Date'
+                            ]
+                        }, (1000*60*60*24)
+                   ]
+                }
+            }
+        }, 
+        {
+            '$match': {
+                'daysince': { "$gt": 0, "$lte":1}
+            }
+        },
+        {
+          '$sample': {'size': 5}  
+        }])
+
+        for datas in results:
+            print("######################")
+            print(datas)
+            lang = datas.get("Language")
+            word = datas.get("Word")
+            
+            for data in datas:
+                print(data.rjust(20,' ') ," : ", datas[data])
+
+            self.update_first_repeat_date(lang,word)    
+            
+            print("######################")
+        
+
+    def get_second_repeat_words(self):
+        results = self.vocabularies.aggregate([
+        {
+            '$match': {
+                'First Learning Date': {
+                    '$ne': None
+                }, 
+                'First Repeat': {
+                    '$ne': None
+                }, 
+                'Second Repeat' : None
             }
         }, {
             '$project': {
@@ -164,52 +286,108 @@ class Vocabulary:
                                 '$subtract': [
                                     datetime.combine(date.today(),datetime.min.time()) , '$First Learning Date'
                                 ]
-                            }, 86400000
+                            }, (1000*60*60*24) 
                         ]
                     #}
                 }
             }
         }, {
             '$match': {
-                'daysince': { "$gt": 0, "$lte":1}
+                'daysince': { "$gt": 2, "$lte":3}
             }
-        }])
+        },
+        {'$sample': {'size': 5}}])
 
         for datas in results:
             print("######################")
-            # print(datas)
+            print(datas)
             lang = datas.get("Language")
             word = datas.get("Word")
             
             for data in datas:
                 print(data.rjust(20,' ') ," : ", datas[data])
 
-
-            self.update_first_repeat_date(lang,word)    
+            self.update_second_repeat_date(lang,word)    
             
             print("######################")
-        
-        #print(datetime.combine(date.today(),datetime.min.time()))
 
-    def get_second_repeat_words():
-        pass
+    def get_third_repeat_words(self):
+        results = self.vocabularies.aggregate([
+        {
+            '$match': {
+                'First Learning Date': {
+                    '$ne': None
+                }, 
+                'First Repeat': {
+                    '$ne': None
+                }, 
+                'Second Repeat' : {
+                    '$ne': None
+                }, 
+                'Third Repeat': None
+            }
+        }
+        , {
+            '$project': {
+                '_id': 0, 
+                'Language': 1, 
+                'Word': 1, 
+                'Word Type': 1, 
+                'Spelling': 1, 
+                'Turkish Meaning': 1, 
+                'English Meaning': 1, 
+                'Usage': 1, 
+                'First Learning Date': 1, 
+                'First Repeat': 1, 
+                'Second Repeat': 1, 
+                'Third Repeat': 1, 
+                'daysince': {
+                    
+                        '$divide': [
+                            {
+                                '$subtract': [
+                                    datetime.combine(date.today(),datetime.min.time()) , '$First Learning Date'
+                                ]
+                            }, (1000*60*60*24) 
+                        ]
+                   
+                }
+            }
+        }
+        , {
+            '$match': {
+                'daysince': { "$gt": 5, "$lte":6}
+            }
+        }
+        ,{'$sample': {'size': 5}}
+        ])
 
-    def get_third_repeat_words():
-        pass
+        for datas in results:
+            print("######################")
+            print(datas)
+            lang = datas.get("Language")
+            word = datas.get("Word")
+            
+            for data in datas:
+                print(data.rjust(20,' ') ," : ", datas[data])
+
+            self.update_third_repeat_date(lang,word)    
+            
+            print("######################")
 
 
 
-lng = 'English'
-wrd = 'Abandon'
-wrd_type = 'Verb'
-spelling = 'Ebendın'
-tr_meaning = 'Terk Etmek, Bırakmak'
-eng_meaning = ''
-usage = 'He had to abandon his car on the side of the road because it had run out of gas.'
-first_learning = None
-first_rpt = None
-scd_rpt = None
-trd_rpt = None
+# lng = 'English'
+# wrd = 'Abandon'
+# wrd_type = 'Verb'
+# spelling = 'Ebendın'
+# tr_meaning = 'Terk Etmek, Bırakmak'
+# eng_meaning = ''
+# usage = 'He had to abandon his car on the side of the road because it had run out of gas.'
+# first_learning = None
+# first_rpt = None
+# scd_rpt = None
+# trd_rpt = None
 
 # add_word (language=lng,word= wrd,word_type=wrd_type,spelling=spelling,turkish_meaning=tr_meaning,english_meaning= eng_meaning,usage=usage,first_learning_date=first_learning,first_repeat=first_rpt,second_repeat=scd_rpt,third_repeat=trd_rpt)
 
@@ -220,7 +398,33 @@ trd_rpt = None
 
 #get_all_words()
 
+# This function gets all daily words: 1 - New Words, 2- First Repeat Words, 3- Second Repeat Words, 4- Third Repeat Words
+# Total Maximum 20 Words to memorize.
 voc = Vocabulary()
 # voc.get_all_words()
-voc.get_first_repeat_words()
-# get_new_words()
+# print("-------------- New Words ------------------")
+# voc.get_new_words()
+# print("-------------- First Repeat ------------------")
+#voc.get_first_repeat_words()
+# print("-------------- Second Repeat ------------------")
+# voc.get_second_repeat_words()
+# print("-------------- Third  Repeat ------------------")
+# voc.get_third_repeat_words()
+# voc.get_new_words()
+
+# voc.add_from_csv("Vocabulary.csv")
+
+# lang = None #"English"
+# word = "Book"
+# filter = {}
+# new_values = {} 
+
+# if (lang != None):
+#     filter.update({"Language":lang})
+# if (word != None):
+#     filter.update({"Word":word})
+
+
+# voc.update_word(lang="English",word="book",nw_wt="Noun",nw_usa="I bought a new book to learn English from beginning")
+# add_word_new(self,language,translate_lang, word,word_type, spelling,meaning, usage):
+voc.add_word_new("English", "Türkçe","Book","Noun", "buuk","Kitap", "I bought a new book to learn English from beginning","htatarli")
